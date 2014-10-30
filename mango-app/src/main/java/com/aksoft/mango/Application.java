3619b6812db01f024ef9958e469b250d9246cf20
qbapmg <qbapmg@gmail.com>
2014-10-30 20:36:55 +0100

    project can be started - jpa starter removed from maven dependencies

--- mango-app/src/main/java/com/aksoft/mango/Application.java
+++ mango-app/src/main/java/com/aksoft/mango/Application.java
@@ -1,5 +1,6 @@
 package com.aksoft.mango;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,9 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        // TODO Implement accessing mongo
     }
 }
